1cfe63dede312788cdc90be644cafea3d9ae82a9
Illari <illari.alvarezgil@gmail.com>
2024-08-16 11:33:22 +0200

    (app): add table

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,35 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './App.css';
+import 'primereact/resources/themes/lara-light-indigo/theme.css'; //theme
+import { PrimeReactProvider } from 'primereact/api';
+import { ComparingPage } from './pages/Comparing';
+
+import { initializeApp } from "@firebase/app";
+import { getFirestore } from "@firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA_MaHjwqlQTEjk9cvCsx27uUarfFLilt4",
+  authDomain: "price-comparator-bc37a.firebaseapp.com",
+  projectId: "price-comparator-bc37a",
+  storageBucket: "price-comparator-bc37a.appspot.com",
+  messagingSenderId: "259969235355",
+  appId: "1:259969235355:web:e233630deeae0b0f84093e"
+};
+
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+
+// Initialize Cloud Firestore and get a reference to the service
+const db = getFirestore(app);
+
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <PrimeReactProvider>
+      <ComparingPage dbUrl={db}/>
+    </PrimeReactProvider>
   )
 }
 
---
+++ src/pages/Comparing.jsx
@@ -0,0 +1,26 @@
+import reactLogo from '../assets/react.svg';
+import viteLogo from '../assets/vite.svg';
+import { useState } from 'react';
+import { collection, getDocs } from "@firebase/firestore";
+import Table from '../components/Table/Index';
+
+export const ComparingPage =  ({dbUrl}) => {
+
+ const [count, setCount] = useState(0);
+ 
+ const getStores = async () => {
+    const querySnapshot = await getDocs(collection(dbUrl, "stores"));
+    querySnapshot.forEach((doc) => {
+        // doc.data() is never undefined for query doc snapshots
+        console.log(doc.id, " => ", doc.data());
+      });
+ }
+
+getStores()
+
+return <>
+    <p>Comparador de precios</p>
+
+    <Table/>
+  </>
+}
